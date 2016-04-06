d50a40e724bc52b5c79abc0c905df8fa3d7aece2
Joe Coleman <jcoleman@ourams.com>
2016-04-06 09:18:02 -0400

    navigate to index on successful submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,28 @@
-import React, { Component } from  'react';
+import React, { Component, PropTypes } from  'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
+	onSubmit(props) {
+		// createpost is an actioncreator that returns a promise
+		this.props.createPost(props)
+			.then(() => {
+				// blog post has been created, navigate user to index
+				// by calling this.context.router.push with new path
+				this.context.router.push('/');
+			});
+	}
 	render(){
 		const { fields: { title, categories, content }, handleSubmit } = this.props;
 		return (
-			<form onSubmit={handleSubmit(this.props.createPost)}>
+			// handleSubmit is a built-in redux-form function
+			// <form onSubmit={handleSubmit(this.props.createPost)}>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<h3>Create A New Post</h3>
 				<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
 					<label>Title</label>
